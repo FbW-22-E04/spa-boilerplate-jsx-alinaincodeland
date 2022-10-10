92556db7e5d64c74376a12a20b79ad8ca1d25cd2
dci-student <alinacuznetov@gmail.com>
2022-10-10 14:45:58 +0200

    solution added

--- src/index.js
+++ src/index.js
@@ -1,21 +1,20 @@
 import ReactDOM from "react-dom/client"; //we use ReactDOM to render into the DOM
 
 // WHAT: This variable returns contains the html to render
+let age = 12;
+let name = "James";
+
 let output = (
-	<span>
-		James is 12 years old
-	</span>
+  <span>
+    {name} is {age} years old
+  </span>
 );
 
 // WHERE: A DOM element that will contain the entire react generated html
-const container = document.getElementById('root');
+const container = document.getElementById("root");
 
 // create a root
 const root = ReactDOM.createRoot(container);
 
 //render app to root
 root.render(output);
-
-
-
-
